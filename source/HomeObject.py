4a7ca17cbef8d5cc539dc7ce521c7e6a6748b2ea
Andri Setiawan <andri.setiawan@gameloft.com>
2020-06-17 18:21:10 +0700

    - setup homeobject

--- source/HomeObject.py
+++ source/HomeObject.py
@@ -17,3 +17,21 @@ class HomeObject:
 	
 	def AddEntryPoint(self, entry):
 		self.entryPoints.append(entry)
+		
+	def IsPartFromThis(self, object):
+		for parts in self.objectParts:
+			if object.Id == parts.Id:
+				return True
+		return False
+	
+	def HasTarget(self):
+		return (len(self.targets) > 0 and self.targets[0] != None)
+	
+	def Occupied(self):
+		if not self.HasTarget():
+			return 0,0
+		occupied = 0
+		for target in self.targets:
+			if not target.available:
+				occupied += 1
+		return occupied,len(self.targets)
