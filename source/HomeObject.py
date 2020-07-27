24f9da7110194a44e092848f82dcf83795d7e30a
Anders Stormhanssens <sethdrewn@gmail.com>
2020-07-27 20:19:58 +0700

    + add more resource, create choice activate agent(WIP)

--- source/HomeObject.py
+++ source/HomeObject.py
@@ -1,6 +1,11 @@
 import Target
 import EntryPoint
 
+ENERGY_TYPE_NONE	= 0
+ENERGY_TYPE_POWER	= ENERGY_TYPE_NONE + 1
+ENERGY_TYPE_WATER	= ENERGY_TYPE_POWER + 1
+ENERGY_TYPE_GAS		= ENERGY_TYPE_WATER + 1
+
 class HomeObject:
 	HomeObjectList = []
 	
@@ -8,6 +13,21 @@ class HomeObject:
 		self.objectParts = []
 		self.targets = []
 		self.entryPoints = []
+		
+		self.m_name = ""
+		self.m_code = ""
+		self.m_initialDate = ""
+		self.m_lifeSpan = 0
+		self.m_size = (0,0,0)
+		self.m_function = ""
+		
+		m_objectCondition = 0
+		m_maintenanceStatus = 0
+		m_cleanliness = 0
+		m_temperature = 0
+		m_appearance = (0,0)
+		
+		m_useEnergyType = ENERGY_TYPE_NONE
 	
 	def AddParts(self, part):
 		self.objectParts.append(part)
