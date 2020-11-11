5129c8e4bc08cebb2b1a18ac8fd04a4a2c4fedc7
Anders Stormhanssens <sethdrewn@gmail.com>
2020-11-11 20:48:15 +0700

    - add activity class

--- source/Activity.py
+++ source/Activity.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 
 class InterruptProperty:
 	def __init__(self, canInterrupt, canBeInterrupted):
@@ -18,28 +19,93 @@ class PlanningProperty:
 		self.m_habit = habit
 		self.m_rule = rule
 
-class BiologicalEnum(Enum):
-	EXHAUSTED	= 0
-	SLEEPY		= 1
-	DIRTY		= 2
-	URINATE		= 3
-	DEFECATE	= 4
-	ENERGY		= 5
-	HUNGER		= 6
-	THIRSTY		= 7
-	ABILITY		= 8
+#biological effect index
+BIO_EXHAUSTED	= 0
+BIO_SLEEPY		= BIO_EXHAUSTED + 1
+BIO_DIRTY		= BIO_SLEEPY + 1
+BIO_URINATE		= BIO_DIRTY + 1
+BIO_DEFECATE	= BIO_URINATE + 1
+BIO_ENERGY		= BIO_DEFECATE + 1
+BIO_HUNGER		= BIO_ENERGY + 1
+BIO_THIRSTY		= BIO_HUNGER + 1
+BIO_ABILITY		= BIO_THIRSTY + 1
+BIO_TOTAL		= BIO_ABILITY + 1
 
+#emotional-social index
+ES_STRESS		= 0
+ES_EMOTION		= ES_STRESS + 1
+ES_MOOD			= ES_EMOTION + 1
+ES_LONELINESS	= ES_MOOD + 1
+ES_TOTAL		= ES_LONELINESS + 1
+
+#------------------------------------------------------------------------------------------------------------
+#-------------------Base Activity Class----------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------
 class Activity:
 	def __init__(self, ID, description):
 		self.m_ID = ID
-		self.m_desxription = description
+		self.m_description = description
 
+#------------------------------------------------------------------------------------------------------------
+#-------------------Core Activity Class----------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------
 class CoreActivity(Activity):
-	def __init__(self, interruptProperty, timeProperty, rooms, planProperty, biologicalEffect)
+	def __init__(self, ID, description, interruptProperty, timeProperty, rooms, planProperty, biologicalEffect, esFactor)
+		Activity.__init__(self, ID, description)
 		self.m_interruptProperty = interruptProperty
 		self.m_timeProperty = timeProperty
 		self.m_rooms = rooms
 		self.m_planProperty = planProperty
 		self.m_biologicalEffect = biologicalEffect
+		self.m_esFactor = esFactor
 	
-	#def SetBiologicalEffect(se)
+	#version using bio effect from agent
+	def CalculateActivityScore(self, agentBiologicalEffect, agentESFactor):
+		#current formula is:
+		# (SUM(bio effect)/COUNT(bio effect))*MULTIPLY(es factor)
+		totalScore = 0
+		bioFactorCount = BIO_TOTAL
+		for i in range(0,BIO_TOTAL):
+			if (self.m_biologicalEffect[i] == 0):
+				bioFactorCount -= 1
+			else
+				totalScore += agentBiologicalEffect[i]
+		
+		totalScore /= bioFactorCount
+		
+		for i in range(0,ES_TOTAL):
+			if self.m_esFactor[i] > 0:
+				totalScore *= agentESFactor[i]
+		
+		return totalScore
+	
+	#version using embedded in activity
+	def CalculateActivityScore(self):
+		#current formula is:
+		# (SUM(bio effect)/COUNT(bio effect))*MULTIPLY(es factor)
+		totalScore = 0
+		bioFactorCount = BIO_TOTAL
+		for i in range(0,BIO_TOTAL):
+			totalScore += self.m_biologicalEffect[i]
+			if (self.m_biologicalEffect[i] == 0):
+				bioFactorCount -= 1
+		
+		totalScore /= bioFactorCount
+		
+		for i in range(0,ES_TOTAL):
+			if self.m_esFactor[i] > 0:
+				totalScore *= m_esFactor[i]
+		
+		return totalScore
+
+#------------------------------------------------------------------------------------------------------------
+#-------------------Support Activity Class-------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------
+class SupportActivity(Activity):
+	def __init__(self, ID, description, duration, habitFactor):
+		Activity.__init__(self, ID, description)
+		self.m_duration = duration
+		self.m_habitFactor = habitFactor
+	
+	def IsActivityExecuted(self):
+		return random.random() < self.m_habitFactor
