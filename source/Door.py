2a2307a8d2279dced70bd6554518b0a4447eaae8
Anders Stormhanssens <sethdrewn@gmail.com>
2020-12-22 20:58:13 +0700

    + add new docs - continue implement door

--- source/Door.py
+++ source/Door.py
@@ -1,14 +1,24 @@
 #import Agent
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+import Rhino as rh
+from Rhino.Geometry import Vector3d
+from ghpythonlib.componentbase import executingcomponent as component
+
 
 #DISTANCE_TOLERANCE
+DOOR_OPEN	= 0
+DOOR_CLOSE	= DOOR_OPEN + 1
 
 class Door:
-	def __init__(self, panel, frameBB, status):
+	def __init__(self, panel, frameBB, rotationCenter, rotDir, status = DOOR_OPEN):
 		self.m_panel = panel
 		self.m_frameBB = frameBB #doorframe bounding box
 		self.m_status = status
+		self.m_rotationCenter = rotationCenter
 		self.m_lock = False
 		self.m_rectTest = None
+		self.m_openAngle = rotDir * 90
 		
 		print(self.m_frameBB)
 	
@@ -17,7 +27,19 @@ class Door:
 		if self.m_lock:
 			return False
 		
+	def Open(self):
+		if self.m_status == DOOR_CLOSE:
+			sc.doc = rh.RhinoDoc.ActiveDoc
+			rs.RotateObject(self.m_panel,self.m_rotationCenter,self.m_openAngle,Vector3d.ZAxis)
+			self.m_status = DOOR_OPEN
+			sc.doc = component.ghdoc
 	
+	def Close(self):
+		if self.m_status == DOOR_OPEN:
+			sc.doc = rh.RhinoDoc.ActiveDoc
+			rs.RotateObject(self.m_panel,self.m_rotationCenter,-self.m_openAngle,Vector3d.ZAxis)
+			self.m_status = DOOR_CLOSE
+			sc.doc = component.ghdoc
 	# def Update(self):
 		# for agent in Agent.agentList:
 			
